8bb3a260c8ca5dc6efb07f2a4555d3831b62b063
Dariel Avila <darielavila43@gmail.com>
2026-09-17 18:30:14 +0800

    feat: update the error in api deployment

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,6 +1,6 @@
 import os
 
-bind = "0.0.0.0:5000"
+bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
 workers = int(os.getenv("WEB_WORKERS", "1"))
 threads = int(os.getenv("WEB_THREADS", "4"))
 timeout = int(os.getenv("REQUEST_TIMEOUT_SECONDS", "120"))
